a16f6f0bfb00bfc483c1a43765516fdcafe3720f
hennyman <hennyman_83@hotmail.com>
2019-05-18 12:12:46 +0300

    first code for player attributes

---
+++ util/player.py
@@ -0,0 +1,100 @@
+class player:
+    
+    def __init__(self):
+        
+        # General
+        self.name = None
+        self.uid = None
+        self.club_name = None
+        self.club_uid = None
+        self.date_of_birth = None
+        self.age = None
+        
+        # International
+        self.international_apps = None
+        self.international_goals = None
+        self.under_21_apps = None
+        self.under_21_goals = None
+        
+        # Contract
+        self.value = None
+        self.basic_wage = None
+        self.contract_expires = None
+        
+        # Form
+        self.condition = None
+        self.form = None
+        self.morale = None
+        self.average_rating = None
+        
+        # Positions
+        self.goalkeeper
+        self.full_back_left
+        self.full_back_right
+        self.defender_central
+        self.wing_back_left
+        self.wing_back_right
+        self.defensive_midfielder
+        self.midfielder_left
+        self.midfielder_right
+        self.midfielder_central
+        self.attacking_midfielder_left
+        self.attacking_midfielder_right
+        self.attacking_midfielder_central
+        self.striker
+        
+        # Goalkeeping Attributes
+        self.aerial_attribute = None
+        self.command_of_area = None
+        self.communication = None
+        self.eccentricity = None
+        self.handling = None
+        self.kicking = None
+        self.one_on_ones = None
+        self.reflexes = None
+        self.rushing_out = None
+        self.tendency_to_punch = None
+        self.throwing = None
+        
+        # Technical Attributes
+        self.corners = None
+        self.crossing = None
+        self.dribbling = None
+        self.finishing = None
+        self.first_touch = None
+        self.free_kicks = None
+        self.heading = None
+        self.long_shots = None
+        self.long_throws = None
+        self.marking = None
+        self.passing = None
+        self.penalty_taking = None
+        self.tackling = None
+        self.technique = None
+        
+        # Mental Attributes
+        self.aggression = None
+        self.anticipation = None
+        self.bravery = None
+        self.composure = None
+        self.concentration = None
+        self.creativity = None
+        self.decisions = None
+        self.determination = None
+        self.flair = None
+        self.influence = None
+        self.off_the_ball = None
+        self.positioning = None
+        self.teamwork = None
+        self.work_rate = None
+        
+        # Physical Attributes
+        self.acceleration = None
+        self.agility = None
+        self.balance = None
+        self.jumping = None
+        self.natural_fitness = None
+        self.pace = None
+        self.stamina = None
+        self.strength = None
+        
